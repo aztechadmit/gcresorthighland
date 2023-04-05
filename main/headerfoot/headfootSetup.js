fea2e319b44a7f9d7839a380b1ca5dfd0a53a5ea
Aztech Ticketing Services <106356042+aztechadmit@users.noreply.github.com>
2023-04-05 11:33:03 -0700

    Update headfootSetup.js

--- main/headerfoot/headfootSetup.js
+++ main/headerfoot/headfootSetup.js
@@ -26,11 +26,11 @@ function initiateHeadFoot(pgNum) {
 function createHeader(typ) {
   if(typ == 0){ //normal desktop header
     //remember to put in order of first item will be the right most, last item will be the left most
-    headerHTML.innerHTML = "<a href='../../index.html'><img class='headerLogo' src='../images/gcresortlogo.png'></a>"
+    headerHTML.innerHTML = "<a href='index.html'><img class='headerLogo' src='https://aztechadmit.github.io/gcresorthighland/main/images/gcresortlogo.png'></a>"
     
-    headerHTML.innerHTML += "<a class='headerLink' href='places-to-stay.html' id='head.pts'>PLACES TO STAY</a>\
+    headerHTML.innerHTML += "<a class='headerLink' href='https://aztechadmit.github.io/gcresorthighland/places-to-stay.html' id='head.pts'>PLACES TO STAY</a>\
                             <a class='headerLink'>THINGS TO DO ↓</a>\
-                            <a class='headerLink' href='index.html' id='head.home'>HOME</a>";
+                            <a class='headerLink' href='https://aztechadmit.github.io/gcresorthighland' id='head.home'>HOME</a>";
     
   }else if(typ==1){ //mobile view header
     
