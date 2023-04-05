e8454a3362f7ee3995f656d7d981b245650bc131
Aztech Ticketing Services <106356042+aztechadmit@users.noreply.github.com>
2023-04-05 11:07:36 -0700

    Update headfootSetup.js

--- main/headerfoot/headfootSetup.js
+++ main/headerfoot/headfootSetup.js
@@ -3,28 +3,53 @@ const footerHTML = document.getElementById('footerDiv');
 
 var typ = 0;
 
-function initiateHeadFoot() {createFooter(); resizeHeader();}
+function initiateHeadFoot(pgNum) {
+  
+  //Step 1: Create the header
+  resizeHeader();
+  createFooter();
+  
+  //step 2, get the page number 0 is the index. Null means no page will be highlighted
+  //This function is to display the page the viewer is currently on as bold in the header
+  var idToSet = 'null';
+  
+  switch(pgNum){
+    case 0:idToSet='head.home';break;
+    case 1:idToSet='head.pts';break;
+    case 'null':break;
+  }//end of switch
+  
+  if(idToSet != 'null'){
+  document.getElementById(idToSet).style.fontWeight = "bold";
+  }
+  
+}//end of initiateHeadFoot
 
 function createHeader(typ) {
   if(typ == 0){ //normal desktop header
-    
+    //remember to put in order of first item will be the right most, last item will be the left most
+    headerHTML.innerHTML = "<a class='headerLink' href='places-to-stay.html' id='head.pts'>PLACES TO STAY</a>\
+                            <a class='headerLink'>THINGS TO DO ↓</a>\
+                            <a class='headerLink' href='index.html' id='head.home'>HOME</a>";
     
   }else if(typ==1){ //mobile view header
     
     
   }
   
-}
+}//end of createHeader
 
 function createFooter() {
-  
-}
+  footerHTML.innerHTML = "This is where the footer will go";
+}//end of createFooter
 
 function resizeHeader() {
-  if (window.innerWidth < 900){
+  if (window.innerWidth < 600){
     createHeader(1);
-  }else{createHeader(2);
-}
+  }else{createHeader(2);}
+}//end of resizeHeader
+  
+
 
 // Adjust the header if the window is resized
 window.onresize = resizeHeader;
