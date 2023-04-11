73c35d2d3ce6bf53a90965f41809ae745cd6f054
Aztech Ticketing Services <106356042+aztechadmit@users.noreply.github.com>
2023-04-11 09:40:32 -0700

    Create resortinfoscript.js

---
+++ main/backpages/scripts/resortinfoscript.js
@@ -0,0 +1,14 @@
+resortInfoTags = {
+  "<p style='color:red;'>Closed</p>",
+  "<p style='color:green;'>Light Crowds</p>",
+  "<p style='color:orange;'>Moderate</p>",
+  "<p style='color:red;'>Busy</p>",
+  //For Parking Lots:
+  "<p style='color:red;'>Closed</p>",
+  "<p style='color:green;'>Lots of Space</p>",
+  "<p style='color:orange;'>Filling Up</p>",
+  "<p style='color:red;'>Limited Space</p>"
+};
+
+resortParkingRates = {"<p>$15 - $28</p>","<p>$24 - $35</p>","<p>$24 - $35</p>","<p>$30 - $50</p>","<p>$30 - $55</p>"};
+
