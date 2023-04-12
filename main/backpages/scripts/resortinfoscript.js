74e18887afd61b62756a48704ea457a426934088
Aztech Ticketing Services <106356042+aztechadmit@users.noreply.github.com>
2023-04-12 10:10:05 -0700

    Update resortinfoscript.js

--- main/backpages/scripts/resortinfoscript.js
+++ main/backpages/scripts/resortinfoscript.js
@@ -1,5 +1,4 @@
-/*
-resortInfoTags = {
+resortInfoTags = [
   "<p style='color:red;'>Closed</p>",
   "<p style='color:green;'>Light Crowds</p>",
   "<p style='color:orange;'>Moderate</p>",
@@ -9,19 +8,26 @@ resortInfoTags = {
   "<p style='color:green;'>Lots of Space</p>",
   "<p style='color:orange;'>Filling Up</p>",
   "<p style='color:red;'>Limited Space</p>"
-};
+];
 
-resortParkingRates = {"<p>$15 - $28</p>","<p>$24 - $35</p>","<p>$24 - $35</p>","<p>$30 - $50</p>","<p>$30 - $55</p>"};
-
-*/
+resortParkingRates = ["<p>$15 - $28</p>","<p>$24 - $35</p>","<p>$24 - $35</p>","<p>$30 - $50</p>","<p>$30 - $55</p>"];
 
 // Functions:
+
+const months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+const days = ['Sunday','Monday','Teusday','Wedensday','Thursday','Friday','Saturday'];
 
 function runResortInfo() {
   
   const dateDisplay = document.getElementById('datedisptxt');
   
   const tdate = new Date();
-  dateDisplay.innerHTML = "<p style='margin-top:0'>Todays Date: " + tdate + "</p>";
+ 
+  const tmonth = months[tdate.getMonth()];
+  const tday = tdate.getDate();
+  const tyear = tdate.getFullYear();
+  const dayOfWeek = days[tdate.getDay()+1];
+  
+  dateDisplay.innerHTML = "<p style='margin-top:0'>"+dayOfWeek+", "+tmonth+" "+tday+" "+tyear+"</p>";
   
 }
