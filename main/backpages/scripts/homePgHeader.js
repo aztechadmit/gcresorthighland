a98cbc36daa9932471169ba19543356038484e42
Aztech Ticketing Services <106356042+aztechadmit@users.noreply.github.com>
2023-04-18 12:20:52 -0700

    Update homePgHeader.js

--- main/backpages/scripts/homePgHeader.js
+++ main/backpages/scripts/homePgHeader.js
@@ -28,6 +28,14 @@ function setUpHomePage() {
 	}
 	
 	// CREATE THE SPOTLIGHT
+	
+	var title1; 	var title2;	 	var title3;
+	var imgLnk1; 	var imgLnk2;	var imgLnk3;
+	var content1;	var content2;	var content3;
+	var link1; 		var link2;	 	var link3;
+	var linkTxt1;	var linkTxt2;	var linkTxt3;
+	
+	
 	switch(tmonth) {
 		case 0:	// January
 			
@@ -39,11 +47,9 @@ function setUpHomePage() {
 			
 			break;
 		case 3: // April
-			spotlightDiv.innerHTML = "<h1>Resort Spotlight</h1><p style='margin-top:0px;'>April "+tdate.getFullYear()+"</p>";
-			spotlightDiv.innerHTML += " <div class='tileContainer'><img class='tileImage' src='https://aztechadmit.github.io/gcresorthighland/main/images/banner1.jpg'\><h3>New Look, Same Great Functionality</h3><p>You may have noticed a fresh new look to our website. We've made some changes around here to help make your experience even better!</p><br><a class='tileLink' href='information'>Learn More</a></div> \
-      									<div class='tileContainer'><img class='tileImage' src='https://aztechadmit.github.io/gcresorthighland/main/images/banner3.jpg'\><h3>It's April</h3><p>Waittimes are low and tickets are cheap; Now is the perfect time for a mid-spring visit to the park!</p><br><a class='tileLink' href='parktickets'>Buy Tickets</a></div> \
-      									<div class='tileContainer'><img class='tileImage' src='https://aztechadmit.github.io/gcresorthighland/main/images/banner5.jpg'\><h3>The Resort is Hiring</h3><p>Help create magic and adventure for millions of guests every year and become a part of something amazing! All this and more with a job at the funnest place in Minecraftia</p><br><a class='tileLink'>Learn More</a></div> \
-										<br><button style='margin:auto;'>View News and Updates</button><br>";
+			title1 = 'Test1';	content1 = 'Test1';	imgLnk1 = 'Test1'; link1 = 'Test1';	linkTxt1 = 'Test1';
+			title2 = '';	content2 = '';	imgLnk2 = ''; link2 = '';	linkTxt2 = '';
+			title3 = '';	content3 = '';	imgLnk3 = ''; link3 = '';	linkTxt3 = '';
 			break;
 		case 4: // May
 			
@@ -70,8 +76,17 @@ function setUpHomePage() {
 			
 			break;
 	}
+	
+	// Build the Resort Spotlight Section
+	spotlightDiv.innerHTML = "<h1>Resort Spotlight</h1><p style='margin-top:0px;'>April "+tdate.getFullYear()+"</p>";
+	spotlightDiv.innerHTML += " <div class='tileContainer'><img class='tileImage' src='" + imgLnk1 + "'\><h3>" + title1 + "</h3><p>" + content1 + "</p><br><a class='tileLink' href='" + link1 + "'>" + linkTxt1 + "</a></div> \
+      							<div class='tileContainer'><img class='tileImage' src='" + imgLnk2 + "'\><h3>" + title2 + "</h3><p>" + content2 + "</p><br><a class='tileLink' href='" + link2 + "'>" + linkTxt2 + "</a></div> \
+								<div class='tileContainer'><img class='tileImage' src='" + imgLnk3 + "'\><h3>" + title3 + "</h3><p>" + content3 + "</p><br><a class='tileLink' href='" + link3 + "'>" + linkTxt3 + "</a></div> \
+								<br><button style='margin:auto;' onclick='window.open("+'"parktickets"'+">View News and Updates</button><br>";
 
 } // function setUpHomePage();
+
+
 
 
 /* SPOTLIGHT EXAMPLE
