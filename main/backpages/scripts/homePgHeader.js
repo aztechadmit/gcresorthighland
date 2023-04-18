bf9c8911f466c5ec3b6e57c39b489cb94ab5d7d7
Aztech Ticketing Services <106356042+aztechadmit@users.noreply.github.com>
2023-04-18 09:42:05 -0700

    Update homePgHeader.js

--- main/backpages/scripts/homePgHeader.js
+++ main/backpages/scripts/homePgHeader.js
@@ -2,22 +2,89 @@ const tdate = new Date();
 
 const tmonth = tdate.getMonth();
 
+const headDiv = document.getElementById('homePageHeaderDiv');
+const spotlightDiv = document.getElementById('homePageSpotlightDiv');
+
+function setUpHomePage() {
+	
+	// CREATE THE VIDEO HEADER
+
+	if(tmonth >= 0 && tmonth < 5){ // JANUARY - MAY
 
 
-if(tmonth >= 0 && tmonth < 5){ // JANUARY - MAY
-    
-    
-    
-}else if(tmonth >= 5 && tmonth < 8){ // JUNE - AUGUST
-    
-    
-    
-}else if(tmonth >= 8 && tmonth < 10) {//SEPTEMBER - OCTOBER
-    
-    
-    
-}else if(tmonth >= 11) { // NOVEMBER - DECEMBER
-    
-    
-    
-}
+
+	}else if(tmonth >= 5 && tmonth < 8){ // JUNE - AUGUST
+
+
+
+	}else if(tmonth >= 8 && tmonth < 10) {//SEPTEMBER - OCTOBER
+
+
+
+	}else if(tmonth >= 11) { // NOVEMBER - DECEMBER
+
+
+
+	}
+	
+	// CREATE THE SPOTLIGHT
+	switch(tmonth) {
+		case 0:	// January
+			
+			break;
+		case 1: // February 
+			
+			break;
+		case 2: // March
+			
+			break;
+		case 3: // April
+			spotlightDiv.innerHTML = "<h1>Resort Spotlight</h1><p style='margin-top:0px;'>April "+tdate.getFullYear()+"</p>";
+			spotlightDiv.innerHTML += " <div class='tileContainer'><img class='tileImage' src='https://aztechadmit.github.io/gcresorthighland/main/images/banner1.jpg'\><h2>New Look, Same Great Functionality</h2><p>You may have noticed a fresh new look to our website. We've made some changes around here to help make your experience even better!</p><a class='tileLink' href='information'>Learn More</a></div> \
+      									<div class='tileContainer'><img class='tileImage' src='https://aztechadmit.github.io/gcresorthighland/main/images/banner3.jpg'\><h1>It's April</h1><p>Waittimes are low and tickets are cheap; Now is the perfect time for a mid-spring visit to the park!</p><a class='tileLink' href='parktickets'>Buy Tickets</a></div> \
+      									<div class='tileContainer'><img class='tileImage' src='https://aztechadmit.github.io/gcresorthighland/main/images/banner5.jpg'\><h1>The Resort is Hiring</h1><p>Help create magic and adventure for millions of guests every year and become a part of something amazing! All this and more with a job at the funnest place in Minecraftia</p><a class='tileLink'>Learn More</a></div> \
+										<br><button style='margin:auto;'>View News and Updates</button><br>";
+			break;
+		case 4: // May
+			
+			break;
+		case 5: // June
+			
+			break;
+		case 6: // July
+			
+			break;
+		case 7: // August
+			
+			break;
+		case 8: // September
+			
+			break;
+		case 9: // October
+			
+			break;
+		case 10: // November
+			
+			break;
+		case 11: // December
+			
+			break;
+	}
+
+} // function setUpHomePage();
+
+
+/* SPOTLIGHT EXAMPLE
+			spotlightDiv.innerHTML = "<h1>Resort Spotlight</h1><p style='margin-top:0px;'>April "+tdate.getFullYear()+"</p>";
+			spotlightDiv.innerHTML += " <div class='tileContainer'><img class='tileImage' src='https://aztechadmit.github.io/gcresorthighland/main/images/banner2.jpg'\><h2>TITLE 1</h2><p>P CONTENTS 1</p><a class='tileLink'>Learn More</a></div> \
+      									<div class='tileContainer'><img class='tileImage' src='https://aztechadmit.github.io/gcresorthighland/main/images/banner1.jpg'\><h1>TITLE 2</h1><p>P CONTNENTS 2</p><a class='tileLink'>Learn More</a></div> \
+      									<div class='tileContainer'><img class='tileImage' src='https://aztechadmit.github.io/gcresorthighland/main/images/banner3.jpg'\><h1>TITLE 3</h1><p>P CONTENTS 3</p><a class='tileLink'>Learn More</a></div> \
+										<br><button style='margin:auto;'>View News and Updates</button><br>";
+			break; 
+*/
+
+
+
+
+
+
