767a97a2f8a71239c9cc89352be0ac1e4d08125c
Aztech Ticketing Services <106356042+aztechadmit@users.noreply.github.com>
2023-04-22 17:08:12 -0700

    Update headfootSetup.js

--- main/headerfoot/headfootSetup.js
+++ main/headerfoot/headfootSetup.js
@@ -206,9 +206,9 @@ function createFooter() {
     // end of footer links
     
     footerHTML.innerHTML += "<div class='lineBreak' style='margin:0;'></div>";
-    footerHTML.innerHTML += "<div style='width:100%;text-align:center;><p><b>Get In Touch</b></p><p>(312) 872 2212</p><p><a>guestservices.gcrh@gcpr.net</a></p></div>";
+    footerHTML.innerHTML += "<div style='width:100%;text-align:center;'><p><b>Get In Touch</b></p><p>(312) 872 2212</p><p><a>guestservices.gcrh@gcpr.net</a></p></div>";
     footerHTML.innerHTML += "<div class='lineBreak' style='margin:0;'></div>";
-    footerHTML.innerHTML += "<br><div style='width:100%;text-align:center;color:gray;><p>&#169;2023 Globe-City Parks and Resorts LLC.</p><p>Subsidiary of Globe-City Entertainment LLC. and R and R Industries<sup>tm</sup></div><br>";
+    footerHTML.innerHTML += "<br><div style='width:100%;text-align:center;color:gray;'><p>&#169;2023 Globe-City Parks and Resorts LLC.</p><p>Subsidiary of Globe-City Entertainment LLC. and R and R Industries<sup>tm</sup></div><br>";
 }//end of createFooter
 
 /*
