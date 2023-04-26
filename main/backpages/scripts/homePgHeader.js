2caaa1c3f4b9c0b0d28332addfccd92c258a70b4
Aztech Ticketing Services <106356042+aztechadmit@users.noreply.github.com>
2023-04-26 11:13:29 -0700

    Update homePgHeader.js

--- main/backpages/scripts/homePgHeader.js
+++ main/backpages/scripts/homePgHeader.js
@@ -121,9 +121,9 @@ function setUpHomePage() {
 	// Build the Resort Spotlight Section
 	spotlightDiv.innerHTML = "<h1>Resort Spotlight</h1><p style='margin-top:0px;'>April "+tdate.getFullYear()+"</p>";
 	spotlightDiv.innerHTML += "<div class='tileSection'>
-	spotlightDiv.innerHTML += " <div class='tileContainer'><img class='tileImage' src='" + imgLnk1 + "'\><h3>" + title1 + "</h3><p>" + content1 + "</p><br><a class='tileLink' href='" + link1 + "'>" + linkTxt1 + "</a></div> \
-      							<div class='tileContainer'><img class='tileImage' src='" + imgLnk2 + "'\><h3>" + title2 + "</h3><p>" + content2 + "</p><br><a class='tileLink' href='" + link2 + "'>" + linkTxt2 + "</a></div> \
-								<div class='tileContainer'><img class='tileImage' src='" + imgLnk3 + "'\><h3>" + title3 + "</h3><p>" + content3 + "</p><br><a class='tileLink' href='" + link3 + "'>" + linkTxt3 + "</a></div></div> \
+	spotlightDiv.innerHTML += " <div class='tileContainer'><img src='" + imgLnk1 + "'\><h3>" + title1 + "</h3><p>" + content1 + "</p><br><a href='" + link1 + "'>" + linkTxt1 + "</a></div> \
+      							<div class='tileContainer'><img src='" + imgLnk2 + "'\><h3>" + title2 + "</h3><p>" + content2 + "</p><br><a href='" + link2 + "'>" + linkTxt2 + "</a></div> \
+								<div class='tileContainer'><img src='" + imgLnk3 + "'\><h3>" + title3 + "</h3><p>" + content3 + "</p><br><a href='" + link3 + "'>" + linkTxt3 + "</a></div></div> \
 								<br><br><button style='margin:auto;' onclick='window.open("+'"main/resort-updates","_self"'+")'>View News and Updates</button><br>";
 
 } // function setUpHomePage();
